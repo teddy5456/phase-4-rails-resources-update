909b046409414c3f56670b2dccfe740d3cb27188
teddy <teddywambua5456@gmail.com>
2023-06-26 01:07:10 +0300

    Completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,15 +1,8 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
-  end
-
-  # POST /birds
-  def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
   end
 
   # GET /birds/:id
@@ -18,14 +11,45 @@ class BirdsController < ApplicationController
     if bird
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render json: { error: 'Bird not found' }, status: :not_found
+    end
+  end
+
+  # POST /birds
+  def create
+    bird = Bird.new(bird_params)
+    if bird.save
+      render json: bird, status: :created
+    else
+      render json: bird.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: 'Bird not found' }, status: :not_found
+    end
+  end
+
+  # PATCH /birds/:id/like
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: 'Bird not found' }, status: :not_found
     end
   end
 
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
-
 end
